e1bad68120e9490886e37eef0cc0e9209d225838
kirstyc <kirstycha@gmail.com>
2018-03-23 15:03:14 -0600

    update from kirsty

--- py_code/kirsty_tries_py/test_plotly.py
+++ py_code/kirsty_tries_py/test_plotly.py
@@ -1,7 +1,8 @@
 #code to test plotly
 import sqlite3
-import plotly,plotly as py
+import plotly.plotly as py
 import time
+import sys
 from plotly.graph_objs import Scatter, Layout, Figure
 
 username = 'kirstycha'
@@ -22,12 +23,15 @@ def plot_it(x_data, y_data):
     print py.plot(fig, filename = 'my plot')
 
 def getHistData (numSamples):
+    conn = sqlite3.connect('sensorsData.db')
+    curs = conn.cursor()
     curs.execute("SELECT * FROM DHT_data ORDER BY timestamp DESC LIMIT "+str(numSamples))
     data = curs.fetchall()
     dates = []
     temps = []
     hums = []
     press = []
+    conn.close()
     for row in reversed(data):
         dates.append(row[0])
         temps.append(row[1])
@@ -36,7 +40,7 @@ def getHistData (numSamples):
     return dates, temps, hums, press
 
 py.sign_in(username, api_key)
-times, temps, hums, press = getHistData(10)
+times, temps, hums, press = getHistData(11)
 plot_it(times, temps)
 stream = py.Stream(stream_token)
 stream.open()
