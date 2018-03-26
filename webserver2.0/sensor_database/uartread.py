b3a7fbce7dc0784f0aefa80ac121818e3484eb6c
lawrencechiu12 <lawrencechiu12@gmail.com>
2018-03-26 14:59:03 -0600

    U0pdated database thing

--- webserver2.0/sensor_database/uartread.py
+++ webserver2.0/sensor_database/uartread.py
@@ -14,12 +14,13 @@ ser.flushInput()
 def logData(status):
 	conn = sqlite3.connect('sensorsData.db')
 	curs = conn.cursor()
-	#curs.execute("INSERT INTO MOTION_stat VALUES(datetime(strftime('now'),(?))",(status))
 	curs.execute("INSERT INTO MOTION_stat VALUES(datetime(strftime('%Y-%m-%d %H:%M:%S','now','localtime')),(?))",(status,))
 	conn.commit()
 	conn.close()
 	time.sleep(5)
 
+status = 0
+prev_status = 0
 while True:
 	if ser.inWaiting() > 0:
 		char=ser.read(1)
@@ -30,17 +31,21 @@ while True:
 
 		if message == "a31BUTTONON-":
 			status = 1
-			logData(status)
-			print(status)
-			message == "\0"
+			if status != prev_status: 
+ 				logData(status)
+				print(status)
+				message == "\0"
 
 		elif message == "a31BUTTONOFF":
 			status = 0
-			logData(status)
-			print(status)
-			message == "\0"
+			if status != prev_status:
+				logData(status)
+				print(status)
+				message == "\0"
 
 		else:
 			print("Waiting for Status")
 			message == "\0"
 			time.sleep(2)
+
+		prev_status = status
