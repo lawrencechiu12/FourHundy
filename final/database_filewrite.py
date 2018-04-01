f88850ee2cc320b9d9a5dc084e7e4fc7de82d1d7
kirstyc <kirstycha@gmail.com>
2018-03-31 21:38:00 -0600

    update # 3006

--- final/database_filewrite.py
+++ final/database_filewrite.py
@@ -2,12 +2,25 @@ import sqlite3
 import time
 import sys
 
+conn = sqlite3.connect('../sensorsData.db')
+curs = conn.cursor()
 
 # This function pulls the latest 6 sets of data from the database
+def check_num():
+	num = maxRowsTable()
+	if num > 6:
+		num = 6
+	return num
+
+def maxRowsTable():
+	for row in curs.execute("SELECT COUNT(temp) from DHT_data"):
+		maxNumberRows = row[0]
+	return maxNumberRows
+
 def getData(num):
-	conn = sqlite3.connect('sensorsData.db')
-	curs = conn.cursor()
-	curs.execute("SELECT * FROM DHT_data ORDER BY timestamp DESC LIMIT "+str(num))
+	#conn = sqlite3.connect('sensorsData.db')
+	#curs = conn.cursor()
+	curs.execute("SELECT * FROM DHT_data ORDER BY time_UTC DESC LIMIT "+str(num))
 	data = curs.fetchall()
 	dates = []
 	temps = []
@@ -19,8 +32,8 @@ def getData(num):
 		temps.append(row[2])
 		hums.append(row[3])
 		pres.append(row[4])
-#	return dates, temps, hums, pres #add in pres later
-	return dates, temps, hums
+	return dates, temps, hums, pres #add in pres later
+#	return dates, temps, hums
 
 #This function  writes the pulled data into a file in a readable format
 def writeData(num, dates, temps, hums, pres):
@@ -30,6 +43,7 @@ def writeData(num, dates, temps, hums, pres):
 		file.write(" " + (str(dates[x])) + "			" + (str(temps[x])) + "		 " + (str(hums[x])) + "		"+(str(pres[x]))+"\n") # add in pressure later
 	file.close()
 
-dates, temps, hums, pres = getData(6)
+numSamples = check_num()
+dates, temps, hums, pres = getData(numSamples)
 #dates, temps, hums = getData(6)
-writeData(6, dates, temps, hums, pres)
+writeData(numSamples, dates, temps, hums, pres)
