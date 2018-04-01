cd9614cdb1bec33edd093b37650409014c1d0cfd
kirstyc <kirstycha@gmail.com>
2018-03-31 19:16:25 -0600

    testing calling py in py

--- final/Scripts/comparedict.py
+++ final/Scripts/comparedict.py
@@ -10,9 +10,9 @@ new_dict = {'dates': None, 'temp': None, 'hum': None, 'pres': None}
 #new_dict = {'dates': None, 'temp': None, 'hum': None}
 
 def getData(num):
-	conn = sqlite3.connect('sensorsData.db')
+	conn = sqlite3.connect('../sensorsData.db')
 	curs = conn.cursor()
-	curs.execute("SELECT * FROM DHT_data ORDER BY timestamp DESC LIMIT "+str(num))
+	curs.execute("SELECT * FROM DHT_data ORDER BY time_UTC DESC LIMIT "+str(num))
 	data = curs.fetchall()
 	dates = []
 	temps = []
@@ -20,10 +20,10 @@ def getData(num):
 	pres = []
 	conn.close()
 	for row in reversed(data):
-		dates.append(row[0])
-		temps.append(row[1])
+		dates.append(row[1])
 		hums.append(row[2])
-		pres.append(row[3])
+		temps.append(row[3])
+		pres.append(row[4])
 
 	return dates, temps, hums, pres
 	#return dates, temps, hums
@@ -33,13 +33,16 @@ def compareData(dates, temps, hums, pres):
 #def compareData(dates, temps, hums):
 	if abs(temps[0] - temps[1]) > 4:
 		print "temp"
-		sudo python /home/pi/FourHundy/final/Scripts/temp_alert.py
+		#sudo python /home/pi/FourHundy/final/Scripts/temp_alert.py
+		import temp_alert
 	if abs(hums[0] - hums[1]) > 5:
 		print "hums"
-		sudo python /home/pi/FourHundy/final/Scripts/hum_alert.py
+		#sudo python /home/pi/FourHundy/final/Scripts/hum_alert.py
+		import hum_alert
 	if abs(pres[0] - pres[1]) > 5:
 		print "pres"
-		sudo python /home/pi/FourHundy/final/Scripts/pres_alert.py
+		#sudo python /home/pi/FourHundy/final/Scripts/pres_alert.py
+		import pres_alert
 	else:
 		print "no alert"
 
